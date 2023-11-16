1da6edeaca3191c16e880f4013dc091459eab324
Jinseo Sung <jinseosung15@gmail.com>
2023-11-16 15:20:17 +0100

    add Root.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Root from "./path/Root";
+import "./styles/main.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/path/Root.js
@@ -0,0 +1,11 @@
+import Main from "../pages/main";
+
+const Root = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
+
+export default Root;
